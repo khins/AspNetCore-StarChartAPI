02a7665ed5ada61ed29ddc6f28a208793eaab455
khins <kevin@hinshawfam.com>
2019-07-31 14:50:18 -0500

    Completed module

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
+
+
